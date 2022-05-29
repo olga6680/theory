4fcb560193238472b46c8ea8b03469cc40412d72
Olga <boa906680@gmail.com>
2022-05-29 18:06:47 +0300

    Массивы и объекты.
    Общение с пользователем через консоль

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMaried: false
+// };
+
+// console.log(obj.name);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++; // оператор инкремента, т.е. увеличения на 1
+// decr--; // оператор декремента, т.е. уменьшения на 1
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(incr++); // постфиксная форма. Возвращает сначала старое значение. Результат будет 10
+// console.log(decr--); //10
+
+console.log(++incr); // префиксная форма. Возвращает новое значение. Результат будет 11
+console.log(--decr); // 9
+
+console.log(5 % 2);
