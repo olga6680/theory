a41bcce7904cab498ee1eddcb59ce4109b75a744
Olga <boa906680@gmail.com>
2022-05-29 19:57:11 +0300

    Практика: циклы с вопросами пользователь

--- 9script.js
+++ 9script.js
@@ -23,34 +23,34 @@
 
 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
 
-    },
-    actors: {},
-    genres: [],
-    privat: false
-};
+//     },
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 
-//Условия 
+// //Условия 
 
-if (4 == 4) {
-    console.log('ok');
-} else {
-    console.log('error');
-}
+// if (4 == 4) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
 
 // const num = 50;
 
@@ -124,14 +124,56 @@ if (4 == 4) {
 //         break;     //прервет цикл на этом условии
 //     }
 //     console.log(i);
-   
+
 // }
 
 //*** */
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        continue;     //пропускает этот шаг в цикле
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         continue; //пропускает этот шаг в цикле
+//     }
+//     console.log(i);
+
+// }
+
+
+//Практика 
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i =0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');  
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b; 
+         console.log('done');
+    } else {
+        console.log('error');
+
+        i--; //Если это условие не выполнилось, мы вернемся на одно повторение
+    }   
+    
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
     }
-    console.log(i);
-   
 }
+
+console.log(personalMovieDB);
