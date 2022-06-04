a1c47162ce7b868a85099a4ddd5b18b02582e637
Olga <boa906680@gmail.com>
2022-06-04 18:39:56 +0300

    Функции

--- 9script.js
+++ 9script.js
@@ -139,41 +139,84 @@
 
 //Практика 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
 
-    },
-    actors: {},
-    genres: [],
-    privat: false
-};
+//     },
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-for (let i =0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');  
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-         personalMovieDB.movies[a] = b; 
-         console.log('done');
-    } else {
-        console.log('error');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
 
-        i--; //Если это условие не выполнилось, мы вернемся на одно повторение
-    }   
-    
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман');
-    } else {
-        console.log('Произошла ошибка');
-    }
+//         i--; //Если это условие не выполнилось, мы вернемся на одно повторение
+//     }
+
+//     if (personalMovieDB.count < 10) {
+//         console.log('Просмотрено довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log('Вы классический зритель');
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log('Вы киноман');
+//     } else {
+//         console.log('Произошла ошибка');
+//     }
+// }
+
+// console.log(personalMovieDB);
+
+// ФУНКЦИИ
+let num = 20;
+
+function showFirstMessage(text) {  // function declaration создается до начала выполнения кода
+    console.log(text);
+    let num = 10;
+    console.log(num);
 }
 
-console.log(personalMovieDB);
+showFirstMessage('Hello World!');
+console.log(num);
+ 
+// function calc(a, b) {
+//     return (a + b);  // return - заканчивает функцию. Все, что дальше уже не сработает
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;  // return возвращает результат работы функции. выводит наружу
+}
+
+const anotherNum = ret(); 
+console.log(anotherNum);
+
+const logger = function() {  // function expression создается только после объявления
+    console.log('Hello!');
+};                              // после такой функции ставим ;
+
+logger();
+
+// Стрелочные функции
+
+// const calc = (a, b) =>  a + b ; //если стрелочная функция в одну строку, можно опускать фигурные скобки
+
+const calc = (a, b) => {
+    console.log('1');
+    return a + b;
+};
