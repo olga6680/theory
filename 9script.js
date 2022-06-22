9cdd9b8e043b60d983a5fe069ce2b3da0f22b002
Olga <boa906680@gmail.com>
2022-06-22 16:13:19 +0300

    Циклы в циклах и метки в циклах

--- 9script.js
+++ 9script.js
@@ -136,6 +136,83 @@
 
 // }
 
+// Цикл в цикле
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// классический пример цикла - пирамидка из звездочек
+
+//*
+//**
+//***
+//****
+//*****
+
+// let result = '';
+// const lengths = 7;
+
+// for (let i = 1; i < lengths; i++) {
+
+//     for (let j = 0; j < 1; j++) {
+//         result += "*";
+//     }
+
+//     result += "\n";
+// }
+
+// console.log(result);
+
+////////// Метки в циклах
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k == 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+
+// }
+
+//получим 
+// First level: 0
+// Second level: 0
+// Third level: 0
+// Third level: 1
+// First level: 1
+// Second level: 0
+// Third level: 0
+// Third level: 1
+// First level: 2
+// Second level: 0
+// Third level: 0
+// Third level: 1
+
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k == 2) break first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+
+// }
+
+//Получим 
+// First level: 0
+// Second level: 0
+// Third level: 0
+// Third level: 1
+
 
 //Практика 
 
@@ -503,46 +580,46 @@
 
 // console.log(border);
 
-console.log(NaN || 2 || undefined);
+// console.log(NaN || 2 || undefined);
 
-console.log(NaN && 2 && undefined);
+// console.log(NaN && 2 && undefined);
 
-console.log(1 && 2 && 3);
-console.log(!1 && 2 || !3);
+// console.log(1 && 2 && 3);
+// console.log(!1 && 2 || !3);
 
-console.log(25 || null && !3);
+// console.log(25 || null && !3);
 
-console.log(NaN || null || !3 || undefined || 5);
+// console.log(NaN || null || !3 || undefined || 5);
 
-console.log(NaN || null && !3 && undefined || 5);
+// console.log(NaN || null && !3 && undefined || 5);
 
-console.log(5 === 5 && 3 > 1 || 5);
-
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger === 3 && cola || fries === 3 && nuggets) {
-    console.log('Done!')
-}
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-
-if (hamburger || cola || fries === 3 || nuggets) {
-    console.log('Done!')
-}
+// console.log(5 === 5 && 3 > 1 || 5);
 
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
-if (hamburger && cola || fries === 3 && nuggets) {
-    console.log('Done!');
-}
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!');
+// }
