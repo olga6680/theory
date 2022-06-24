9bc456bc004e9c41f79abbe43b3d6eadaef84ad4
Olga <boa906680@gmail.com>
2022-06-24 12:41:32 +0300

    029 Передача по ссылке или по значению, Spread оператор (ES6-ES9)

--- 9script.js
+++ 9script.js
@@ -833,12 +833,164 @@ console.log(doNothing() === undefined);
 
 // [ 1, 10, 12, 13, 6 ]
 
-const arr = [1, 12, 13, 6, 10];
-arr.sort(compareNum);
-console.log(arr);
+// const arr = [1, 12, 13, 6, 10];
+// arr.sort(compareNum);
+// console.log(arr);
 
-function compareNum(a, b) { // функция для правильной сортировки чисел
-    return a - b;
-}
+// function compareNum(a, b) { // функция для правильной сортировки чисел
+//     return a - b;
+// }
 
 //  [1, 6, 10, 12, 13]
+
+
+// ПЕРЕДАЧА ПО ССЫЛКЕ ИЛИ ПО ЗНАЧЕНИЮ
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;    
+
+// console.log(b);
+// console.log(a);
+
+// 10
+// 5
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // передает ссылку, а не сам обьект
+
+// copy.a = 10; //поэтому здесь мы модифицируем не копию, а сам обьект, т.е. через ссылку на него
+
+// console.log(copy);
+// console.log(obj);
+
+// { a: 10, b: 1 }
+// { a: 10, b: 1 }
+
+// поэтому для создания копий массивов и обьектов сущществует несколько способов
+
+//1. с помощью цикла
+
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10; //поменяет только в копии
+// newNumbers.c.x = 10; // поменяет в обоих массивах
+
+// // мы создали поверхностную копию. 
+// //Она меняет свойства копий только в родителях, во вложенных структурах нет 
+// //(меняет по ссылке на оригинал)
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+//2. assign() - помещает обьект в обьект
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// // console.log(Object.assign(numbers, add));
+
+// //{ a: 2, b: 5, c: { x: 7, y: 4 }, d: 17, e: 20 }
+
+// //чтобы просто создать копию обьекта можно поместить в него пустой обьект
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// { d: 17, e: 20 }
+// { d: 20, e: 20 }
+
+// Копирование массивов
+//3. метод slice()
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'dfaj';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+// //4. Оператор разворота spreat оператор
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejournal', 'blogger'],
+//       internet = [...video, ...blogs, 'instagram', 'facebook'];
+
+// console.log(internet);      
+
+// [
+//     'youtube',
+//     'vimeo',
+//     'rutube',
+//     'wordpress',
+//     'livejournal',
+//     'blogger',
+//     'instagram',
+//     'facebook'
+//   ]
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+// 2
+// 5
+// 7
+
+const array = ["a", "b"];
+
+const newArray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q };
