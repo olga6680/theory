e6c22cc2719f43ccd9953e66ca12aaf1dd7cc7a8
Olga <boa906680@gmail.com>
2022-07-03 10:29:43 +0300

    MATE Количество букв

---
+++ mate/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+// Создай функцию countMs, которая принимает строку text и возвращает количество букв 'm' в ней (и больших и маленьких вместе).Создай функцию countMs, которая принимает строку text и возвращает количество букв 'm' в ней (и больших и маленьких вместе).
+
+// const countMs = (text) => {
+//     let count = 0;
+
+//     for (const letter of text) {
+//       if (letter.toLowerCase() === 'm') ++count;
+//     }
+
+//     return count;
+//   }
+
+// countMs('document') === 1
+// countMs('Mermaid') === 2 // нужно считать и 'm', и 'M'
+// countMs('code') === 0
+
+////////////////////////
